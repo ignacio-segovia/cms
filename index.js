49b0adf9316d201af823446f5fa93f4be5e0bfef
Eze <Riccaesteban@gmail.com>
2024-11-28 00:27:04 -0300

    aqui te mando unos cambios dime si puedes entenderlos

--- index.js
+++ index.js
@@ -16,28 +16,38 @@ function addWorkCard(params = {}) {
 }
 
 function getWorks() {
-  return fetch(
-    "https://cdn.contentful.com/spaces/55xwwgra6ibw/environments/master/entries?access_token=wj23hQX26G7udtx0PWwbWwUZzSw1OSLul0y-Vpan4rw"
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      const fieldsCollections = data.items.map((item) => {
-        return {
-          title: item.fields.titulo,
-          description: item.fields.descripcion,
-          image: item.fields.imagen,
-          url: item.fields.url,
-        };
-      });
-      return fieldsCollections;
+  const url =
+    "https://cdn.contentful.com/spaces/55xwwgra6ibw/environments/master/entries?access_token=wj23hQX26G7udtx0PWwbWwUZzSw1OSLul0y-Vpan4rw";
+  // const urlFetch = await fetch(url);
+  // const dataJson = await urlFetch.json();
+  // const imagenCMS = await dataJson.includes.Asset;
+  const urlFetch = fetch(url);
+  const dataJosn = urlFetch.then((res) => res.json());
+  dataJosn.then((res) => console.log(res));
+
+  return dataJosn.then((data) => {
+    // Mapeamos los items para obtener los datos necesarios
+    const fieldsCollections = data.items.map((item) => {
+      const assetId = item.fields.imagen.sys.id;
+      const asset = data.includes.Asset.find(
+        (asset) => asset.sys.id === assetId
+      );
+      const imageUrl = asset.fields.file.url;
+      return {
+        title: item.fields.titulo,
+        description: item.fields.descripcion,
+        image: imageUrl,
+        url: item.fields.url,
+      };
     });
+    return fieldsCollections;
+  });
 }
 
 function main() {
   getWorks().then(function (works) {
     for (const w of works) {
+      console.log(w);
       addWorkCard(w);
     }
   });
